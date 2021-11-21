665730aea2bc307344ff489da79c78eae6ec7cec
775262 <775262@SAIT227542IT.ACDM.DS.SAIT.CA>
2021-11-21 16:48:34 -0700

    AdminFilter added

--- src/java/services/AccountService.java
+++ src/java/services/AccountService.java
@@ -2,12 +2,13 @@ package services;
 
 import dataaccess.UserDB;
 import models.User;
+import models.Role;
 
 public class AccountService {
-    
+
     public User login(String email, String password) {
         UserDB userDB = new UserDB();
-        
+
         try {
             User user = userDB.get(email);
             if (password.equals(user.getPassword())) {
@@ -15,7 +16,17 @@ public class AccountService {
             }
         } catch (Exception e) {
         }
-        
+
         return null;
+    }
+
+    public int userRoleID(String email) {
+        UserDB userDB = new UserDB();
+        try {
+            User user = userDB.get(email);
+            return user.getRole().getRoleId();
+        } catch (Exception e) {
+            return -1;
+        }
     }
 }
